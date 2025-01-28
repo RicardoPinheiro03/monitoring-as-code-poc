fe5300bceb8151800fc5d98763e35d5783ee8695
Ricardo Pinheiro <r.pinheiro@Ricardos-MBP.lan>
2025-01-28 11:46:44 +0000

    Add services B and C on the metrics generation script

--- metrics_generation/metrics_gen.py
+++ metrics_generation/metrics_gen.py
@@ -4,16 +4,38 @@ import random, time
 LOWER_THRESHOLD_RNG = 0
 UPPER_THRESHOLD_RNG = 100
 STEP_RNG = 1
+METRICS_COUNTER = 500
 
-registry = CollectorRegistry()
+global_registry = CollectorRegistry() # Global registry creation
 
-g_svc_a = Gauge("svc_a_cpu_usage", "CPU Usage Service A", registry=registry)
+cpu_usage = Gauge('service_cpu_usage', 'CPU usage of services', ['service'], registry=registry)
+ram_usage = Gauge('service_ram_usage', 'RAM usage of services', ['service'], registry=registry)
 
 i = 0
-while i < 50:
-        rand_number = random.randrange(LOWER_THRESHOLD_RNG, UPPER_THRESHOLD_RNG, STEP_RNG)
-        print(f"Number generated for CPU Usage in Service A: {rand_number}")
-        g_svc_a.set(rand_number)
+# While cycle to generate artificial CPU and RAM metrics to be injected in Prometheus
+while i < METRICS_COUNTER:
+        cpu_usage.labels(service='service_A').set(generateNumberZeroToHundred())
+        ram_usage.labels(service='service_A').set(generateNumberZeroToHundred())
+        time.sleep(3)
+        cpu_usage.labels(service='service_B').set(generateNumberZeroToHundred())
+        ram_usage.labels(service='service_B').set(generateNumberZeroToHundred())
+        time.sleep(3)
+        cpu_usage.labels(service='service_C').set(generateNumberZeroToHundred())
+        ram_usage.labels(service='service_C').set(generateNumberZeroToHundred())
         time.sleep(3)
         i += 1
-        push_to_gateway("http://localhost:9091", job="cpu_metrics_job", registry=registry)
+        push_to_gateway('http://localhost:9091', job='backend_services', registry=global_registry)
+
+def generateNumberZeroToHundred():
+        """
+        Generates a random number within a range of 0 and 100.
+
+        Args:
+          No input arguments.
+
+        Returns:
+          num: Integer randomly generated between 0 and 100.
+
+        """
+        num = random.randrange(LOWER_THRESHOLD_RNG, UPPER_THRESHOLD_RNG, STEP_RNG)
+        return num
